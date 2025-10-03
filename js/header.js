6fac7b1a7aeb3c4f88a43ac8838a3cd00779e38f
jxmerorozco-debug <jxmerorozco@gmail.com>
2025-10-03 16:16:33 -0500

    PROYECTO CONTACTO

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
 document.querySelector('.encabezado-principal').innerHTML = `
-    <h1>IED TECNICO DE CARRIZAL </h1>
+    <h1>IED TECNICA DE CARRIZAL</h1>
     <nav>
         <a href="../index.html">Inicio</a>
-        <a href="../vistas/registrar_estudiante.html">Estudiantes</a>
-        <a href="contacto.html">Contacto</a>
-        <a href="login.html">Login</a>
+        <a href="../vistas/registrar_estudiantes.html">Estudiantes</a>
+        <a href="../vistas/contactos.html">Contacto</a>
+        <a href="../vistas/login.html">Login</a>
     </nav>
-`
+`;
